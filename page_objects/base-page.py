ab43dc410c1ef960850f38724dabee62f2aba281
Daniel Rocha <daniel.rocha@outlook.com.br>
2023-06-21 21:15:17 -0300

    creation of base-page class

---
+++ page_objects/base-page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+class BasePage():
+
+    def __init__(self, driver: WebDriver):
+        self._driver = driver
+    
+    @property
+    def current_url(self) -> str:
+        return self._driver.current_url
+    
+    def _open_url(self, url: str):
+        self._driver.get(url)
+    
+    def _find(self, locator: tuple):
+        return self._driver.find_element(*locator)
+
+    def _type(self, locator: tuple, text: str,  time: int = 10):
+        self._wait_until_element_is_visible(locator, time)
+        self._find(locator).send_keys(text)
+
+    def _clear(self, locator: tuple, time: int = 10):
+        self._wait_until_element_is_clickable(locator, time)
+        return self._find(locator).clear()
+    
+    def _click(self, locator: tuple, time: int = 10):
+        self._wait_until_element_is_visible(locator, time)
+        self._find(locator).click()
+    
+    
