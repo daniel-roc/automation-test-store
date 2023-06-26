6cf66e01055b07a796a90ef2cf24d37ab265680f
Daniel Rocha <daniel.rocha@outlook.com.br>
2023-06-26 16:56:18 -0300

    feature: implementation of Faker library

--- test_data/account_data.py
+++ test_data/account_data.py
@@ -1,7 +1,7 @@
 account_info = {
     'first_name': 'John',
     'last_name': 'Doe',
-    'email': 'johndoe@email.com',
+    # 'email': 'johndoe@email.com',
     'telephone': '1234567890',
     'fax': '9876543210',
     'company': 'ABC Company',
@@ -11,9 +11,9 @@ account_info = {
     'region_state': 'Alaska',
     'zip_code': '12345',
     'country': 'United States',
-    'login_name': 'johndoe1234',
-    'password': 'Password123',
-    'password_confirm': 'Password123',
+    # 'login_name': 'johndoe1234',
+    # 'password': 'Password123',
+    # 'password_confirm': 'Password123',
     'subscribe': 'Yes',
     'privacy_policy': 'Yes'
 }
--- tests/test_register_account_positive.py
+++ tests/test_register_account_positive.py
@@ -6,13 +6,18 @@ from test_data.account_data import account_info
 
 class TestPositiveScenarios:
 
+    # 1- Test case: Valid input
+
     @pytest.mark.register
     @pytest.mark.debug
-    @pytest.mark.parametrize('first_name, last_name, email, telephone, fax, company, address1, address2, city, region_state, zip_code, country, login_name, password, password_confirm, subscribe, privacy_policy', [account_info.values()])
-    def test_valid_input(self, driver, first_name, last_name, email, telephone, fax, company, address1, address2, city, region_state, zip_code, country, login_name, password, password_confirm, subscribe, privacy_policy):
+    @pytest.mark.parametrize('first_name, last_name, telephone, fax, company, address1, address2, city, region_state, zip_code, country, subscribe, privacy_policy', [account_info.values()])
+    def test_valid_input(self, driver, faker, first_name, last_name, telephone, fax, company, address1, address2, city, region_state, zip_code, country, subscribe, privacy_policy):
         register_page = RegisterAccountPage(driver)
         register_page.open()
-        register_page.type_form(first_name, last_name, email, telephone, fax, company, address1, address2, city, region_state, zip_code, country, login_name, password, password_confirm, subscribe, privacy_policy)
+        username = faker.user_name()
+        password = faker.password()
+        email = faker.email()
+        register_page.type_form(first_name, last_name, email, telephone, fax, company, address1, address2, city, region_state, zip_code, country, username, password, password, subscribe, privacy_policy)
         account_created_page = AccountCreatedPage(driver)
         assert register_page.current_url == account_created_page.expected_url, "Actual URL is not the same as expected."
         
