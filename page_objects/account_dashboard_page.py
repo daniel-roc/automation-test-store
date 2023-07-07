4874ed88da428084bc758fdc9a40a74d7597def3
Daniel Rocha <daniel.rocha@outlook.com.br>
2023-07-07 15:07:41 -0300

    new test: logoff successfully

--- page_objects/account_dashboard_page.py
+++ page_objects/account_dashboard_page.py
@@ -8,6 +8,8 @@ class AccountDashboardPage(BasePage):
 
     __url = "https://automationteststore.com/index.php?rt=account/account"
     __header_locator = (By.XPATH, "//h1//span[@class='maintext']")
+    __logoff_button = (By.XPATH, "//ul[@id='customer_menu_top']/li/ul//a[@href='https://automationteststore.com/index.php?rt=account/logout']")
+    __dropdown_account_menu = (By.CLASS_NAME, "menu_text")
 
     def __init__(self, driver: WebDriver):
         super().__init__(driver)
@@ -17,3 +19,7 @@ class AccountDashboardPage(BasePage):
     
     def get_header_text(self) -> str:
         return super()._get_text(self.__header_locator)
+    
+    def click_logoff_button(self):
+        super()._hover_over(self.__dropdown_account_menu)
+        super()._click(self.__logoff_button)
--- tests/test_login_positive.py
+++ tests/test_login_positive.py
@@ -3,6 +3,7 @@ from page_objects.account_dashboard_page import AccountDashboardPage
 from page_objects.forgot_password_page import ForgotPasswordPage
 
 from page_objects.login_page import LoginPage
+from page_objects.logout_page import LogoutPage
 
 
 class TestPositiveScenarios:
@@ -20,7 +21,6 @@ class TestPositiveScenarios:
     
     @pytest.mark.login
     @pytest.mark.positive
-    @pytest.mark.debug
     def test_reset_password_successfully(self, driver):
         login_page = LoginPage(driver)
         login_page.open()  
@@ -31,3 +31,15 @@ class TestPositiveScenarios:
         forgot_password_page.click_continue_button()
         assert login_page.current_url == login_page.get_url(), "Actual URL is not the same as expected."
         assert login_page.get_success_message() == "×\nSuccess: Password reset link has been sent to your e-mail address."
+    
+    @pytest.mark.login
+    @pytest.mark.positive
+    def test_logoff_successfully(self, driver):
+        login_page = LoginPage(driver)
+        login_page.open()
+        login_page.type_user_credentials("johndoe1234", "Password123")
+        login_page.click_login_button()
+        account_dashboard_page = AccountDashboardPage(driver)
+        account_dashboard_page.click_logoff_button()
+        logout_page = LogoutPage(driver)
+        assert account_dashboard_page.current_url == logout_page.get_url(), "Actual URL is not the same as expected."
